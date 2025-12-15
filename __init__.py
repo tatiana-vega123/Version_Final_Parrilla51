a40269427fbe7fa1d6016958eec2bc79fdff4418
Tatiana <gisellvega820@gmail.com>
2025-12-15 01:45:38 -0500

    base de datos

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@ def create_app():
     app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'mysql.railway.internal')  # Dirección correcta del host
     app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')  # Usuario 'root'
     app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'UBoOKapMxkCvNzRUdLnXZwMdzMmlSdel')  # Contraseña de la base de datos directamente
-    app.config['MYSQL_DB'] = os.getenv('MYSQL_DATABASE', 'parrilla51')  # Nombre de la base de datos
+    app.config['MYSQL_DB'] = os.getenv('MYSQL_DATABASE')  # No pongas 'parrilla51' a menos que sea exacto
     app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))  # Puerto por defecto
     app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
