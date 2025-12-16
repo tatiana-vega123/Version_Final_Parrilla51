32b5430557fae208eb08e72165462b715da658e8
tatiana-vega123 <gisellvega820@gmail.com>
2025-12-16 14:11:49 -0500

    Cambios init

--- __init__.py
+++ __init__.py
@@ -9,12 +9,15 @@ import os
 
 mysql = MySQL()
 mail = Mail()
+serializer = URLSafeTimedSerializer(
+    os.environ.get("SECRET_KEY", "pinchellave")
+)
 
 def create_app():
     app = Flask(__name__, template_folder="template")
     app.secret_key = os.environ.get("SECRET_KEY", "pinchellave")
 
-    # ---------- MYSQL (RAILWAY) ----------
+    # ---------- MYSQL ----------
     app.config['MYSQL_HOST'] = os.environ.get('MYSQLHOST')
     app.config['MYSQL_USER'] = os.environ.get('MYSQLUSER')
     app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQLPASSWORD')
