c90d8f5076f434964ff16b4523c25916c3859d7d
tatiana-vega123 <gisellvega820@gmail.com>
2025-12-16 15:23:37 -0500

    correo

--- __init__.py
+++ __init__.py
@@ -29,7 +29,7 @@ def create_app():
     mysql.init_app(app)
 
     # ---------- MAIL ----------
-  app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = 'enviodecorreosparrilla51@gmail.com'
